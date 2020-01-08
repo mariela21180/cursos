efcfbd490eceb94e92b6c0936e20b427369617c8
Mariela Gonzalez <mariela21180@gmail.com>
2020-01-07 23:07:05 -0300

    #3 - Menu - HTML y CSS

--- react-angular-vuejs/compare-vue/src/App.vue
+++ react-angular-vuejs/compare-vue/src/App.vue
@@ -1,26 +1,75 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            MyCompany
+          </a>
+          <a
+            role="button"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+
+          <div class="navbar-end">
+            <router-link to="/" class="navbar-item r-item">Home</router-link>
+            <router-link to="faq" class="navbar-item r-item">Features</router-link>
+            <router-link to="faq" class="navbar-item r-item">About</router-link>
+            <router-link to="faq" class="navbar-item r-item">FAQ</router-link>
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary is-outlined">
+                  <span class="icon">
+                    <i class="fa fa-download"></i>
+                  </span> 
+                  <span>
+                    JOIN NOW
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style lang="sass">
 @import '../node_modules/bulma/bulma.sass' 
 @import 'mq'
 
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px 
+.navbar
+  background-color: #383838
+  a:hover
+    color: gray
+  .navbar-brand
+    .navbar-item
+      color: #fff
+      font-weight: bold
+  a.r-item
+    color: #c1c1c1  
+    padding: 0.5rem 1.75rem
+    +mobile
+      color: gray
+      &:hover
+        background-color: #f1f1f1
 
 </style>

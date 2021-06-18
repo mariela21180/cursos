3e862c0a925ffabc8308b85d822c09a1f113aab5
Mariela Gonzalez <mariela21180@gmail.com>
2021-06-18 14:49:51 -0300

    Section 4 - Listas de objetos desde JSON

--- aprender-reactjs/src/components/Listas.js
+++ aprender-reactjs/src/components/Listas.js
@@ -1,12 +1,32 @@
 import { Component } from 'react';
+import cars from '../data/cars.json'
+
+class CarItem extends Component {
+  render() {    
+    const {car} = this.props
+    return (
+      <li>
+        {car.name}
+      </li>
+    )
+  }
+}
 
 export default class Listas extends Component {
   render() {
     const numbers = [1, 1, 3, 4]
     return (
         <div>
-            Trabajando con Listas
+            <h4>Trabajando con Listas de números en un Array</h4>
             {numbers.map((number,index)=> <p key={index}>Soy el número {number}</p>)}
+
+            
+            <h4>Trabajando con Listas de Objetos en un JSON</h4>
+            <ul>
+              {cars.map( car => {
+                return <CarItem key={car.id} car={car}/>
+              })}
+            </ul>
             
         </div>
     )

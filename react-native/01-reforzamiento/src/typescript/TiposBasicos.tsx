765c6fc8c028d76d20ab662a74b66369b1ee07c2
Mariela Gonzalez <mariela21180@gmail.com>
2023-04-20 13:12:48 -0300

    react-native > Reforzamiento > Tipos Básicos

--- react-native/01-reforzamiento/src/typescript/TiposBasicos.tsx
+++ react-native/01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,7 +1,16 @@
 export const TiposBasicos = () => {
+
+  const nombre: string = 'Mariela';
+  const edad: number = 42;
+  const estaActivo: boolean = true;
+
+  const poderes: string[] = [ 'Velocidad', 'Teletransportación', 'Energía']
   return (
     <>
       <h3>TiposBasicos</h3>
+      { nombre }, { edad }, { (estaActivo) ? 'activo' : 'no activo' }
+      <br/>
+      { poderes.join(', ')}
     </>
   )
 }

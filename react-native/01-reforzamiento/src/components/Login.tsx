13f5735dec5443bc5e4b9d0b30fc170d59964791
Mariela Gonzalez <mariela21180@gmail.com>
2023-05-09 19:50:18 -0300

    react-native > Reforzamiento > Login con Reducer

--- react-native/01-reforzamiento/src/components/Login.tsx
+++ react-native/01-reforzamiento/src/components/Login.tsx
@@ -1,25 +1,86 @@
+import { useEffect, useReducer } from "react";
+
+interface AuthState {
+  validando: boolean;
+  token: string | null;
+  username: string;
+  nombre: string;
+}
+
+const initialState: AuthState = {
+  validando: true,
+  token: null,
+  username: '',
+  nombre: ''
+}
+
+type AuthAction = { type: 'logout' }
+
+const authReducer = ( state: AuthState, action: AuthAction ): AuthState => {
+  // el state nunca tiene que mutar, es constante
+  switch (action.type) {
+    case 'logout':
+      return {
+        validando: false,
+        token: null,
+        username: '',
+        nombre: ''
+      }
+  
+    default:
+      return state;
+  }
+}
+
 export const Login = () => {
+
+  const [state, dispatch] = useReducer(authReducer, initialState)
+  
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: 'logout' })
+    }, 1500);
+  }, [])
+
+  if (state.validando) {
+    return (
+      <>
+        <h3>Login</h3>
+        <div className="alert alert-info">
+          Validando...
+        </div>
+      </>
+    )
+  }
+  
   return (
     <>
       <h3>Login</h3>
 
-      <div className="alert alert-info">
-        Validando...
-      </div>
-      <div className="alert alert-danger">
-        No Autenticado
-      </div>
-      <div className="alert alert-success">
-      Autenticado
-      </div>
-
-      <button className="btn btn-primary">
-        Login
-      </button>
-      
-      <button className="btn btn-danger">
-        Logout
-      </button>
+      {state.token
+        ? <>
+            <div className="alert alert-success">
+            Autenticado
+            </div>
+            <button
+              className="btn btn-danger"
+              // onClick={}
+              >
+              Logout
+            </button>
+          </>
+        : <>
+            <div className="alert alert-danger">
+              No Autenticado
+            </div>
+            <button
+              className="btn btn-primary"
+              // onClick={}
+              >
+              Login
+            </button>
+          </>
+      }      
     </>
   )
 }

abb415f4ca69cca869c79732828aed91a3255fb5
Mariela Gonzalez <mariela21180@gmail.com>
2023-05-09 20:08:42 -0300

    react-native > Link a curso

--- react-native/01-reforzamiento/src/App.tsx
+++ react-native/01-reforzamiento/src/App.tsx
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <div className='mt-2'>
       <h1>Introducción a TS - React</h1>
+      <a href="https://drive.google.com/drive/folders/1DlXLxoXCUZNZa-P3TA3P8kqeIln3NiIC" target="_blank" rel="noopener noreferrer">Curso Reac Native en Drive de Mariano Menchon</a>
       <hr/>
       {/* <TiposBasicos /> */}
       {/* <ObjetosLiterales /> */}

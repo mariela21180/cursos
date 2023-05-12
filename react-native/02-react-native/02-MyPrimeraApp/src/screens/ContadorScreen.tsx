c545227cccbc1cc8bc493323d81ab67739d4eecb
Mariela Gonzalez <mariela21180@gmail.com>
2023-05-12 17:15:44 -0300

    react-native > Creando proyecto MyPrimeraApp - Contador - Moviendo estilos inline a un StyleSheet

--- react-native/02-react-native/02-MyPrimeraApp/src/screens/ContadorScreen.tsx
+++ react-native/02-react-native/02-MyPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,38 +1,41 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useState } from 'react';
 
 export const ContadorScreen = () => {
     const [contador, setContador] = useState(10);
   return (
-    <View style={{
-        flex: 1,
-        justifyContent: 'center',
-      }}>
-        <Text style={{
-          fontSize: 40,
-          textAlign: 'center'
-        }}>Contador: {contador}</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>Contador: {contador}</Text>
         <TouchableOpacity
             onPress={() => setContador(contador + 1)}
         >
-            <View style={{
-                backgroundColor: 'red',
-                borderRadius: 100
-            }}>
+            <View style={styles.button}>
                 <Text>+1</Text>
             </View>
         </TouchableOpacity>
         <TouchableOpacity
             onPress={() => setContador(contador - 1)}
         >
-            <View style={{
-                backgroundColor: 'red',
-                borderRadius: 100
-            }}>
+            <View style={styles.button}>
                 <Text>-1</Text>
             </View>
         </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 40,
+        textAlign: 'center'
+    },
+    button: {
+        backgroundColor: 'red',
+        borderRadius: 100,
+    },
+  });
